c6b1943c322a47e5963df2486e91674b3a3e6a44
roshansingha <roshansinghaagt@gmail.com>
2020-06-29 17:54:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.start();
+
 function App() {
+
+  const[count, setCount] = useState(0);
+
+  const voiceCommands = () => {
+    recognition.onstart = () => {
+      console.log('Voice is Activated');
+    }
+
+    recognition.onresult =(e) => {
+      let current = e.resultIndex;
+      let transcript = e.results[current][0].transcript;
+      let mobileRepeatBug = (current === 1 && transcript === e.results[0][0].transcript);
+
+      if(!mobileRepeatBug) {
+        if(transcript === 'next' || transcript === ' next') {
+          setCount(count + 1);
+        }
+
+        if(transcript === 'back' || transcript === ' back') {
+          setCount(count - 1);
+        }
+      }
+      setTimeout(() => {
+        recognition.start();
+      }, 50);
+    }
+
+    recognition.onspeedend = () => {
+      recognition.stop();
+      console.log('Voice Stopped');
+    }
+  }
+
+  useEffect(() => {
+    voiceCommands();
+  } );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 }
